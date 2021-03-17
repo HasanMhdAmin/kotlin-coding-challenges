44229373b07b46c43142c24e41ff541e87e70dc5
Hasan Amin <hasan_amin@deichmann.com>
2021-03-17 10:44:19 +0100

    add java file

---
+++ app/src/main/java/com/igorwojda/codingpuzzle/newInTown/JavaImp.java
@@ -0,0 +1,16 @@
+package com.igorwojda.codingpuzzle.newInTown;
+
+import java.util.ArrayList;
+
+public class JavaImp {
+
+    public static int addUpTo(int n) {
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum=sum+i;
+        }
+        return sum;
+    }
+
+
+}
